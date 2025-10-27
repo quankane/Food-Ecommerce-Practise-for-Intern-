71d950b03287c0263280459904f3f07afc3f5b50
quankane <quanducbui2017@gmail.com>
2025-10-27 15:33:27 +0700

    Update dotenv config to set property for system

---
+++ src/main/java/vn/quankane/food_ecommerce/config/DotenvInitializer.java
@@ -0,0 +1,18 @@
+package vn.quankane.food_ecommerce.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class DotenvInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
+}
