18b3035413ce1b306634d410f35a67724bb6af2c
quankane <quanducbui2017@gmail.com>
2025-10-27 16:41:57 +0700

    Update security Config without custom prefilter

--- src/main/java/vn/quankane/food_ecommerce/config/SecurityConfig.java
+++ src/main/java/vn/quankane/food_ecommerce/config/SecurityConfig.java
@@ -1,7 +1,114 @@
 package vn.quankane.food_ecommerce.config;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import vn.quankane.food_ecommerce.constant.RoleConstant;
+import vn.quankane.food_ecommerce.security.CustomUserDetails;
+import vn.quankane.food_ecommerce.security.CustomUserDetailsService;
+
+import javax.swing.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @Configuration
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor()
 public class SecurityConfig {
+
+    CustomUserDetailsService customUserDetailsService;
+
+    @Value("${security.public-endpoints}")
+    @NonFinal()
+    static String[] PUBLIC_ENDPOINTS;
+
+    @Value("${security.user-endpoints}")
+    @NonFinal()
+    static String[] USER_ENDPOINTS;
+
+    @Value("${security.admin-endpoints}")
+    @NonFinal()
+    static String[] ADMIN_ENDPOINTS;
+
+    @Value("${security.swagger-endpoints}")
+    @NonFinal()
+    static String[] SWAGGER_ENDPOINTS;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                        .requestMatchers(PUBLIC_ENDPOINTS).permitAll()
+                        .requestMatchers(SWAGGER_ENDPOINTS).permitAll()
+                        .requestMatchers(USER_ENDPOINTS).hasAnyAuthority(RoleConstant.ADMIN, RoleConstant.USER)
+                        .requestMatchers(ADMIN_ENDPOINTS).hasAuthority(RoleConstant.ADMIN)
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(customizePreFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "https://ahistorical-undelusory-soren.ngrok-free.dev",
+                "https://sandbox.vnpayment.vn",
+                "https://test-payment.momo.vn"));
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return urlBasedCorsConfigurationSource;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return customUserDetailsService;
+    }
 }
+
---
+++ src/main/java/vn/quankane/food_ecommerce/constant/RoleConstant.java
@@ -0,0 +1,8 @@
+package vn.quankane.food_ecommerce.constant;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public class RoleConstant {
+    public static final String ADMIN = "ADMIN";
+    public static final String USER = "USER";
+}
