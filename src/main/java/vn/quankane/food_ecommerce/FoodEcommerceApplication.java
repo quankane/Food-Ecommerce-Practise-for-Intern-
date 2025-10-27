bd1cf2f73f5ee84283d31045be259d95823c7a62
quankane <quanducbui2017@gmail.com>
2025-10-27 11:19:48 +0700

    Initial commit

---
+++ src/main/java/vn/quankane/food_ecommerce/FoodEcommerceApplication.java
@@ -0,0 +1,13 @@
+package vn.quankane.food_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/quankane/food_ecommerce/FoodEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.quankane.food_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
